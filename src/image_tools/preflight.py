4ac3d2b3637ff7cd395963986aae8a1dcb884566
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2024-04-23 17:28:46 +0200

    remove prefix before passing to preflight

--- src/image_tools/preflight.py
+++ src/image_tools/preflight.py
@@ -74,7 +74,9 @@ def preflight_commands(images: List[str], args: Namespace, conf) -> Dict[str, Co
         if args.architecture:
             cmd_args.extend(
                 ["--platform",
-                    args.architecture,
+                    # this argument value has already been checked against valid values with an expected prefix.
+                    # Preflight provides the same "linux/" prefix and so it must be removed here.
+                    args.architecture.split("linux/")[1],
                  ]
             )
         result[img] = Command(args=cmd_args)
